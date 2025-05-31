1dcce629b9c13bdfb79b3567d6f8276f05e1b57d
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-05-31 19:50:55 +0530

    removed  types from types file and added to where it is used

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,17 +1,17 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
 import JWT from "../jwt";
 
-export interface IAuth {
-    secret: string;
-    tokename: string;
-}
-
-function authMiddleware(authParams:IAuth):RequestHandler{
+/**
+ * 
+ * @param {{secret: string, tokenname: string}} authParams 
+ * @returns 
+ */
+function authMiddleware({secret, tokenname}:{secret: string, tokenname: string}):RequestHandler{
     return async(req:Request,res:Response,next:NextFunction)=>{
         try {
-            const token=req.headers.authorization?.split(" ")[1]||req.headers[authParams.tokename]||req.body[authParams.tokename]||req.params[authParams.tokename]||req.query[authParams.tokename]||"";
+            const token=req.headers.authorization?.split(" ")[1]||req.headers[tokenname]||req.body[tokenname]||req.params[tokenname]||req.query[tokenname]||"";
             if(!token) throw new Error("Unauthorised");
-            const data=await JWT.verifyToken({authSecret:authParams.secret,token});         
+            const data=await JWT.verifyToken({authSecret:secret,token});         
             req.user=data;
             next();
         } catch (error:unknown) {
--- types/index.d.ts
+++ types/index.d.ts
@@ -3,9 +3,25 @@ declare module "expresspro" {
     import * as express from 'express';
     export = express;
 }
-declare namespace Express{
-    export interface Request {
-      user?: object;
-    }
-}
   
+import "express";
+import type * as Bcrypt from "bcrypt";
+declare module "express"{
+      interface Express{
+            bcrypt: Bcrypt;
+            errorHandler: RequestHandler;
+            auth(authParams: IAuth): RequestHandle;
+            cors(): RequestHandler;
+            resp(res:Response,status: number,message: string,data: object):Response;
+            async(fn: (req:Request, res:Response, next:NextFunction)=>Promise<any>):RequestHandler;
+      }
+      export interface Request {
+        user?: object;
+      }
+      export function resp(res:Response,status: number,message: string,data: object):Response;
+      export function cors(): RequestHandler;
+      export function auth(authParams: IAuth): RequestHandler;
+      export const errorHandler: RequestHandler;
+      export let bcrypt: Bcrypt;
+      export function async(fn: (req:Request, res:Response, next:NextFunction)=>Promise<any>):RequestHandler;
+}
