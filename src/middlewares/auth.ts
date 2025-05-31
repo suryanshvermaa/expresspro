259d1bc1b88f49b5c7a94d1423ae12d4615eb93f
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-05-31 13:25:29 +0530

    added auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import { IAuth } from "../../types/Express/";
+import JWT from "../jwt";
+
+function authMiddleware(authParams:IAuth):RequestHandler{
+    return async(req:Request,res:Response,next:NextFunction)=>{
+        try {
+            const token=req.headers.authorization?.split(" ")[1]||req.headers[authParams.tokename]||req.body[authParams.tokename]||req.params[authParams.tokename]||req.query[authParams.tokename]||"";
+            if(!token) throw new Error("Unauthorised");
+            const data=await JWT.verifyToken({authSecret:authParams.secret,token});         
+            req.user=data;
+            next();
+        } catch (error:unknown) {
+            next(error);
+        }
+        
+    }  
+}
+
+export default authMiddleware;
