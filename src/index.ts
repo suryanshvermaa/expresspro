1338a8493104eb4cda3106e69b4e27981d52964e
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-05-31 13:26:22 +0530

    added cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,8 @@
-import express,{RequestHandler,Express,Request,Response,NextFunction} from 'express';
-import jwt from 'jsonwebtoken';
+import express,{RequestHandler,Request,Response,NextFunction} from 'express';
 import cors from 'cors';
 
 function corsMiddleware(): RequestHandler {
     return cors();
 }
 
-interface IAuth{
-    secret: string;
-    tokename: string;
-}
 
-function authMiddleware(authParams:IAuth):RequestHandler{
-    return (req:Request,res:Response,next:NextFunction)=>{
-        try {
-            const token=req.headers.authorization?.split(" ")[1]||req.headers[authParams.tokename]||req.body[authParams.tokename]||req.params[authParams.tokename]||req.query[authParams.tokename]||"";
-            if(!token) throw new Error("token not found");
-        } catch (error:unknown) {
-            next(error);
-        }
-        
-    }
-}
