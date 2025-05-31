bf13fb3ce7381fb65920e2aca7c21ca15b8b730f
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-05-31 13:29:39 +0530

    exported auth middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import express,{RequestHandler,Request,Response,NextFunction} from 'express';
+import express,{RequestHandler} from 'express';
 import cors from 'cors';
+import authMiddleware from './middlewares/auth';
 
 function corsMiddleware(): RequestHandler {
     return cors();
 }
 
+express.cors = corsMiddleware;
+express.auth = authMiddleware;
 
+//for common js
+module.exports=express;
+//for modular js/ts
+export default express;
