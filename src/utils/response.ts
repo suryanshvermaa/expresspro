a49c7b90a6fe210bab6373a3859a28538bb6766a
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-05-31 14:35:43 +0530

    added response object function

---
+++ src/utils/response.ts
@@ -0,0 +1,25 @@
+import { Response } from "express";
+import { IRes } from "express-response";
+
+/**
+ * @description: response function to send response
+ * @param res - express response object
+ * @param status - http status code
+ * @param message - response message
+ * @param data - response data
+ * @returns - express response object
+ */
+const response = (
+	res: Response,
+	status: number,
+	message: string,
+	data: object
+) => {
+	const resObj: IRes = {
+		success: true,
+		message,
+		data: data,
+	};
+	return res.status(status).json(resObj);
+};
+export default response;
